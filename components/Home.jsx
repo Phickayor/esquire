a6f4cf86c30bd85dbde1fd7556aa1751c723dc55
Fikayo Jetawo <jetawof@gmail.com>
2023-01-06 23:58:21 +0100

    Adding home component

---
+++ components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='bg-gray-900'>
+            <img
+                src="https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=2000"
+                alt="Background Image"
+                className="w-full opacity-70 lg:h-[80vh]" />
+        </div>
+    )
+}
+
+export default Home
