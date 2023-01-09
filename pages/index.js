51fdd17d7138cd4de20044d52055ac46c16cbd15
Fikayo Jetawo <jetawof@gmail.com>
2023-01-09 11:17:53 +0100

    Adding link to rooms page frome index

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,12 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import Home from '../components/Home'
 import Welcome from '../components/Welcome'
-import Address from '../components/Address'
-import Rooms from '../components/Rooms'
-import Image from 'next/image'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className=''>
       <Home />
       <Welcome />
-      {/* <Rooms /> */}
-      {/* <Address /> */}
     </div>
   )
 }
