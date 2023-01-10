8c57b28f3e625609f0fe8d863eb83ddaa00be2d1
Fikayo Jetawo <jetawof@gmail.com>
2023-01-10 15:11:31 +0100

    Mission and Vision component

---
+++ components/Goals.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Goals() {
+    return (
+
+        <div className='lg:flex mx-auto lg:w-10/12 lg:mt-20 mt-10 lg:space-y-0 space-y-10'>
+            <div className=' lg:px-10  text-center mx-auto w-8/12'>
+                <h1 className="lg:text-4xl text-3xl mb-5 font-semibold text-slate-700">Our Vision</h1>
+                <p className='text-md leading-[35px] '>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam possimus neque obcaecati deleniti accusantium veritatis
+                    fuga error vero sequi eveniet, temporibus laudantium,
+                    quam voluptate et tempore nesciunt? Rem, eum iste.
+                </p>
+            </div>
+            <div className=' lg:px-10 lg:border-l-2 text-center mx-auto w-8/12'>
+                <h1 className="lg:text-4xl text-3xl mb-5 font-semibold text-slate-700">Our Mission</h1>
+                <p className='text-md leading-[35px] '>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam possimus neque obcaecati deleniti accusantium veritatis
+                    fuga error vero sequi eveniet, temporibus laudantium,
+                    quam voluptate et tempore nesciunt? Rem, eum iste.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Goals
