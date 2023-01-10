382e4d2552990adac9684440747908fdfa752687
Fikayo Jetawo <jetawof@gmail.com>
2023-01-10 01:11:33 +0100

    Adding room page and component

--- components/Head.jsx
+++ components/Head.jsx
@@ -4,14 +4,14 @@ function Head(props) {
     const pageTitle = props.pageTitle
     const imageLink = props.imageLink
     return (
-        <div className='h-60 bg-gray-700 rounded-b-2xl'>
+        <div className='h-40 bg-gray-700'>
             <img
-                className=' absolute h-60 rounded-b-2xl w-full opacity-60'
+                className=' absolute h-40 lg:rounded-b-full rounded-b-[40%] w-full opacity-60'
                 src={imageLink}
                 alt={pageTitle + "image"}
             />
-            <div className='flex flex-col justify-end h-60 '>
-                <h1 className="relative font-semibold font-[poppins] text-center text-white text-8xl">{pageTitle}</h1>
+            <div className='flex flex-col lg:justify-end justify-center h-40 '>
+                <h1 className="relative font-semibold font-[poppins] text-center text-white lg:text-8xl text-6xl ">{pageTitle}</h1>
             </div>
         </div>
     )
--- pages/rooms.js
+++ pages/rooms.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import Footer from '../components/Footer'
 import Head from '../components/Head'
+import Rooms from '../components/Rooms'
 function rooms({ pageTitle, imageLink }) {
     pageTitle = "Our Rooms"
     imageLink = "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=2000"
 
     return (
-        <div className='bg-slate-400 text-white'>
+        <div className='bg-slate-700 text-white'>
             <Head
                 pageTitle={pageTitle}
                 imageLink={imageLink}
             />
-
+            <Rooms />
+            <Footer />
         </div>
     )
 }
