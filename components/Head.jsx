5201fc0e9d398f1f8ddb5093a20e2d6de3970373
Fikayo Jetawo <jetawof@gmail.com>
2023-01-10 15:34:08 +0100

    Adjusments in the Head component border radius

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,17 +6,17 @@ function Head(props) {
     const pageTitle = props.pageTitle
     const imageLink = props.imageLink
     return (
-        <div className='h-40 bg-gray-700 sticky top-0'>
+        <div className='h-40 bg-gray-700 lg:rounded-b-full rounded-b-[40%] sticky top-0'>
             <img
                 className=' absolute h-40 lg:rounded-b-full rounded-b-[40%] w-full object-cover opacity-60'
                 src={imageLink}
                 alt={pageTitle + "image"}
             />
-            <button className='absolute border-2 my-5 lg:mx-10 mx-5 px-4 py-2 rounded-2xl text-6xl font-bold border-purple-500' >{menu}</button>
+            <button className='absolute border-2 my-5 lg:mx-10 mx-5 text-white px-4 py-2 rounded-2xl text-6xl font-bold border-purple-500' >{menu}</button>
             <div className='flex flex-col justify-end h-40 '>
-                <h1 className="relative font-semibold font-[poppins] text-center m-2 text-white lg:text-8xl text-6xl ">{pageTitle}</h1>
+                <h1 className="relative font-semibold font-[poppins] text-center my-2 text-white lg:text-8xl text-6xl ">{pageTitle}</h1>
             </div>
-        </div>
+        </div >
     )
 }
 
--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import AboutUs from '../components/AboutUs'
 import Footer from '../components/Footer'
+import Goals from '../components/Goals'
 import Head from '../components/Head'
+import Services from '../components/Services'
 
 function about({ pageTitle, imageLink }) {
     pageTitle = "About Us"
     imageLink = "/background/bg7.jpeg"
 
     return (
-        <div className='text-white'>
+        <div>
 
             <Head
                 pageTitle={pageTitle}
                 imageLink={imageLink}
             />
-
+            <AboutUs />
+            <img className='z-[-9999] opacity-30 lg:hidden left-0 mx-auto w-full  absolute' src="/background/logo.png" />
+            <Goals />
+            <Services />
             <Footer />
         </div>
     )
