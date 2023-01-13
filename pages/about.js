3faf34b85f443f162b934bcf7db45dc50856d754
Fikayo Jetawo <jetawof@gmail.com>
2023-01-13 16:25:33 +0100

    adding navBar to about page

--- pages/about.js
+++ pages/about.js
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AboutUs from '../components/AboutUs'
 import Footer from '../components/Footer'
 import Goals from '../components/Goals'
 import Head from '../components/Head'
+import NavBar from '../components/NavBar'
 import Services from '../components/Services'
 
-function about({ pageTitle, imageLink }) {
+function About({ pageTitle, imageLink, sideWidth }) {
+    const [marginValue, setMarginValue] = useState("0px")
+    const [blurValue, setBlurValue] = useState("")
     pageTitle = "About Us"
     imageLink = "/background/bg7.jpeg"
+    sideWidth = marginValue
 
+    function marginParent(margin, blur) {
+        setMarginValue(margin)
+        setBlurValue(blur)
+    }
     return (
         <div>
-
+            <div>
+                <NavBar
+                    sideWidth={sideWidth}
+                    shadowColor={"slate-700"}
+                    textColor={"slate-700"}
+                />
+            </div>
             <Head
                 pageTitle={pageTitle}
                 imageLink={imageLink}
+                marginHandler={marginParent}
             />
-            <AboutUs />
-            {/* <img className='z-[-9999] opacity-30 lg:hidden left-0 mx-auto w-full  absolute' src="/background/logo.png" /> */}
-            <Goals />
-            <Services />
-            <Footer />
+            <div className={`ml-[${marginValue}] duration-300 ${blurValue}`}>
+                <AboutUs />
+                {/* <img className='z-[-9999] opacity-30 lg:hidden left-0 mx-auto w-full  absolute' src="/background/logo.png" /> */}
+                <Goals />
+                <Services />
+                <Footer />
+            </div>
         </div>
     )
 }
 
-export default about
+export default About
