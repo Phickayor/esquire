3a08f6380e3aed90c84e404381e41d0321180ab9
Fikayo Jetawo <jetawof@gmail.com>
2023-01-13 16:25:07 +0100

    adding navBar to room page

--- pages/rooms.js
+++ pages/rooms.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Head from 'next/head'
 import Footer from '../components/Footer'
 import Head from '../components/Head'
-import Rooms from '../components/Rooms'
-function rooms({ pageTitle, imageLink }) {
+import NavBar from '../components/NavBar'
+import OurRooms from '../components/OurRooms'
+function Rooms({ pageTitle, imageLink, sideWidth }) {
     pageTitle = "Our Rooms"
     imageLink = "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=2000"
-
+    const [blurValue, setBlurValue] = useState("")
+    const [marginValue, setMarginValue] = useState("0px")
+    sideWidth = marginValue
+    function marginParent(margin, blur) {
+        setMarginValue(margin)
+        setBlurValue(blur)
+    }
     return (
         <div className='bg-slate-700 text-white'>
 
@@ -16,14 +23,23 @@ function rooms({ pageTitle, imageLink }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head> */}
+
+            <NavBar
+                sideWidth={sideWidth}
+                shadowColor={"white"}
+                textColor={"white"}
+            />
             <Head
                 pageTitle={pageTitle}
                 imageLink={imageLink}
+                marginHandler={marginParent}
             />
-            <Rooms />
-            <Footer />
+            <div className={`ml-[${marginValue}] ${blurValue}`}>
+                <OurRooms />
+                <Footer />
+            </div>
         </div>
     )
 }
 
-export default rooms
+export default Rooms
